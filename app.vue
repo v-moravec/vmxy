5fde002e0acbcc749f03c770f34f6769094f111a
Vojtěch Moravec <voj.moravec@seznam.cz>
2023-01-13 23:09:20 +0100

    CF Functions test.

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 // const title = 'Jagu Template'
 // const description = 'A Nuxt starter template used by Jagu'
 // const ogImage = 'jagu-template-og-image.png'
@@ -16,5 +16,9 @@
 //   description,
 //   ogImage
 // })
+
+const {data: res} = await useFetch(window.origin + '/helloworld?/jagu')
+
+console.log(res)
 
 </script>
--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,23 @@
-export function onRequest(context) {
-  return new Response('Hello, world!')
+export async function onRequestGet(context) {
+  const { request, env } = context
+
+  const url = new URL(request.url)
+
+  if(url.pathname === '/helloworld') {
+    const redirects = {
+      '/jagu': 'https://www.jagu.cz'
+    }
+
+    await Promise.all(Object.keys(redirects).map(async path => {
+      await env.VMXY_REDIRECTS.put(path, redirects[path])
+    }))
+    console.log(new Response('Data Saved Successfully!'))
+
+    for (let key of url.searchParams.keys()) {
+      const redirect = await env.VMXY_REDIRECTS.get(key)
+      if (redirect) {
+        return new Response(redirect)
+      }
+    }
+  }
 }
