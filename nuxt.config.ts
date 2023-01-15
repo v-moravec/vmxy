222927fa19d3c322f105ee806e4acea1f7501f90
Vojtěch Moravec <voj.moravec@seznam.cz>
2023-01-15 21:52:53 +0100

    CF 405 error test.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'node-server'
+    preset: 'cloudflare-pages'
   },
   ssr: true,
   modules: [
