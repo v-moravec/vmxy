82fcdfe91da97df0a2867d38d7d9cdf026ee20b3
Vojtěch Moravec <voj.moravec@seznam.cz>
2023-01-16 15:01:46 +0100

    Fix CF 405.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
+  nitro: {
+    preset: 'node-server'
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts'
